adbbfaa4750a1b8986662ac3635b6cf83a41103e
aassaf <aymen.assaf@edu.uca.ma>
2023-11-03 11:08:32 +0100

    ft_tolower.c

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 int ft_toupper(int c)
 {
-        if(c >= 'a' && c <= 'a')
+        if(c >= 'a' && c <= 'z')
                 c -= 32;
         return (c);
 }
-#include <stdio.h>
+/* #include <stdio.h>
 int main()
 {
         char c = 'c';
         printf("%c", ft_toupper(c));
-}
+} */
