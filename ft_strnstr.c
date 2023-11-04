8894548fb684187633106e0f54ae8a66b27655d4
aassaf <aymen.assaf@edu.uca.ma>
2023-11-04 11:34:38 +0100

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: aassaf <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 21:40:10 by aassaf            #+#    #+#             */
-/*   Updated: 2023/11/03 22:42:24 by aassaf           ###   ########.fr       */
+/*   Updated: 2023/11/04 11:31:10 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,26 @@ char    *ft_strnstr(const char *haystack, const char *needle, size_t n)
         size_t  j;
 
         i = 0;
-        if(needle[j] == '\0')
-                return (haystack);
+        if(needle[0] == '\0')
+                return ((char *)haystack);
         while(haystack[i] && i < n)
         {
-                if(ft_strncmp(haystack, needle, n) == )
+                j = 0;                
+                while(haystack[i + j] == needle[j] && haystack[i + j] != '\0' && (i + j) < n)
+                        j++;
+                if(needle[j] == '\0')
+                        return((char *)haystack + i);
+                i++;
         }
+        return (NULL);
 }
-// ft_strncmp
+
+#include <stdio.h>
+#include <strings.h>
+int main()
+{
+        const char	to_find[6] = "perl";
+        const char	str[20] = "la perl";
+	printf ("%s", ft_strnstr (str, to_find, 7));
+	// printf ("\n%s", strnstr (str, to_find, 2));       
+}
