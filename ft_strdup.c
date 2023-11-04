9545df7f0d441569a8ce051f582a5ccf5564fa83
aassaf <aymen.assaf@edu.uca.ma>
2023-11-04 18:18:03 +0100

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aassaf <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 14:07:17 by aassaf            #+#    #+#             */
+/*   Updated: 2023/11/04 18:17:05 by aassaf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*new;
+	char	*d;
+
+	i = 0;
+	d = ((new = malloc(sizeof(char) * ft_strlen(s) + 1)));
+	if (!d)
+		return (NULL);
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+/* #include <string.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		printf("%s\n", ft_strdup(av[1]));
+		printf("%s", strdup(av[1]));
+	}
+} */
