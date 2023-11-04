d33a50f38a8d0a8f5c31b7b8f4cadbe0e8d7a206
aassaf <aymen.assaf@edu.uca.ma>
2023-11-04 14:04:32 +0100

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aassaf <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 12:12:42 by aassaf            #+#    #+#             */
+/*   Updated: 2023/11/04 14:03:39 by aassaf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	long	res;
+	int		sign;
+
+	res = 0;
+	sign = 1;
+	if (str == NULL)
+		return (0);
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign *= -1;
+	if (*str == '+' || *str == '-')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + *str - '0';
+		str++;
+	}
+	return (res * sign);
+}
+
+/* #include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	char *str = "           -+12342sp24321";
+	printf ("%d", ft_atoi(str));
+	printf ("\n%d", atoi(str));
+} */
