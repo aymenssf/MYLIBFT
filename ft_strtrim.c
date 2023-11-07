1aa7cf57d9c86cf2274312c7d434679cbb08ef32
aassaf <aymen.assaf@edu.uca.ma>
2023-11-07 20:54:51 +0100

    update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,34 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aassaf <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 01:30:40 by marvin            #+#    #+#             */
-/*   Updated: 2023/11/07 01:30:40 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/07 19:51:46 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 char *ft_strtrim(char const *s1, char const *set)
 {
     size_t i;
     char *ptr;
-
+    size_t new_len;
+    size_t start;
+    size_t end;
+    ptr = NULL;
+    
     i = 0;
     if(s1 == NULL )
         return(NULL);
-    if(set == NULL)
+
+    if(s1[i])
     {
-	    while (*s1 == 32 || (*s1 >= 9 && *s1 <= 13))
-		        s1++;
-    }
-    else
-    {
-        while(*s1)
+        start = 0;
+        while ((ft_strchr(set, s1[start])))
+            start++;
+        end = ft_strlen(s1) - 1;
+        while ((ft_isalnum(s1[end]) == 0) && (ft_strchr(set, s1[end])))
+            end--;
+        new_len = end - start;
+        ptr = (char *)malloc(new_len) + 1;
+        while(i < new_len)
         {
-            if(ft_strchr(set, *s1) == NULL)
-                break;
-            s1++;
+            ptr[i] = s1[start];
+            i++;
+            start++;
         }
+        ptr[i] = '\0';
     }
+    return (ptr);
+}
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[] = "  *aymen";
+    char s[] = "*";
+    printf("%s", ft_strtrim(str, s));
 }
