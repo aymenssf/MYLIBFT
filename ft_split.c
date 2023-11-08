944d68b4f958bfa16d59c597a7ea835505800109
aassaf <aymen.assaf@edu.uca.ma>
2023-11-08 23:38:01 +0100

    ft_split on work

--- ft_split.c
+++ ft_split.c
@@ -10,7 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 char **ft_split(char const *s, char c)
 {
-    
+        size_t      i;
+        size_t len;
+        char **str;
+
+        len = ft_strlen(s) - 1;
+
+        if(!s)
+                return (NULL);
+
+        str = (char **)malloc(len * sizeof(s));
+        while(s[i])
+        {
+                str = ft_strdup((char *)s[i]);
+                str = c;
+                i++;
+        }
+        str = '\0';
+        return (str);
+}
+
+#include <stdio.h>
+
+int main()
+{
+    char **s;
+    int i  = 0;
+    s = ft_split("  AYMEN  ",' ');
+    //printf("%d\n",strings[0]);
+     while (s[i] != NULL) {
+        printf("%s\n", s[i]);
+        i++;
+    }
+    return 0;
 }
