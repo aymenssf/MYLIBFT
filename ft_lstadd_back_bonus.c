d18a3acc72288c3687dfddd1c412c05905df2dd7
aassaf <aymen.assaf@edu.uca.ma>
2023-11-16 12:45:11 +0100

    ft_lstadd_back donee

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,14 +6,34 @@
 /*   By: aassaf <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 19:41:55 by aassaf            #+#    #+#             */
-/*   Updated: 2023/11/15 19:59:48 by aassaf           ###   ########.fr       */
+/*   Updated: 2023/11/16 12:44:25 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 void ft_lstadd_back(t_list **lst, t_list *new)
 {
+        t_list *lastone;
+
         if(!lst || !new)
-                return (NULL);
-        if()
+                return ;
+        lastone = ft_lstlast(*lst);
+        if(lastone == NULL)
+                *lst = new;
+        else
+                lastone -> next = new;       
 }
+/* #include <stdio.h>
+int main()
+{
+        t_list *L = NULL;
+        t_list *n1 = ft_lstnew("aymen");
+        t_list *n2 = ft_lstnew("assaf");
+        ft_lstadd_back(&L, n2);
+        ft_lstadd_back(&L, n1);
+        while(L)
+        {
+                printf("%s\t", (char *)L -> content);
+                L = L -> next;
+        }
+} */
