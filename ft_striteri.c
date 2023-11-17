7099c4445ab8fe2c1a0e80d3f191857188d2247d
Aymen ASSAF <aymen.assaf@edu.uca.ma>
2023-11-17 12:56:51 +0100

    comment main

--- ft_striteri.c
+++ ft_striteri.c
@@ -25,31 +25,31 @@ void ft_striteri(char *s, void (*f)(unsigned int, char*))
                 i++;
         }
 }
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-void	ft_striteri_ft(unsigned int	i, char	*c)
-{
-	*c = *c + i;
-}
+// void	ft_striteri_ft(unsigned int	i, char	*c)
+// {
+// 	*c = *c + i;
+// }
 
-int	main(void)
-{
-	char	str0[] = "Hello world";
-	char	str1[] = "1337 Benguerir";
-	char	str2[] = "";
-	char	str3[] = "0000000000";
-	char	str4[] = "0000000000";
+// int	main(void)
+// {
+// 	char	str0[] = "Hello world";
+// 	char	str1[] = "1337 Benguerir";
+// 	char	str2[] = "";
+// 	char	str3[] = "0000000000";
+// 	char	str4[] = "0000000000";
 
-	ft_striteri(str0, ft_striteri_ft);
-	ft_striteri(str1, ft_striteri_ft);
-	ft_striteri(str2, ft_striteri_ft);
-	ft_striteri(str3, ft_striteri_ft);
-	ft_striteri(str4, 0);
-	printf("%s\n", str0);
-	printf("%s\n", str1);
-	printf("%s\n", str2);
-	printf("%s\n", str3);
-	printf("%s\n", str4);
-	return (EXIT_SUCCESS);
-}
+// 	ft_striteri(str0, ft_striteri_ft);
+// 	ft_striteri(str1, ft_striteri_ft);
+// 	ft_striteri(str2, ft_striteri_ft);
+// 	ft_striteri(str3, ft_striteri_ft);
+// 	ft_striteri(str4, 0);
+// 	printf("%s\n", str0);
+// 	printf("%s\n", str1);
+// 	printf("%s\n", str2);
+// 	printf("%s\n", str3);
+// 	printf("%s\n", str4);
+// 	return (EXIT_SUCCESS);
+// }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -16,6 +16,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
+	i = 0;
 	if (!src || !dst)
 		return (0);
 	while (src && i < size)
