df02570ba9375d58c476fb9b04399e0dda3b3bf7
aymenssf <aymen.assaf@edu.uca.ma>
2023-11-19 13:57:53 +0100

    fix ascii problem

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -20,9 +20,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] > s2[i])
-			return (1);
+			return ((unsigned char)s1[i] - (unsigned char)(s2[i]));
 		else if (s1[i] < s2[i])
-			return (-1);
+			return ((unsigned char)s1[i] - (unsigned char)(s2[i]));
 		i++;
 	}
 	return (0);
